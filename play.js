6969d5e4ff85027a10b84fdbaaba38f49a6de99a
Jayz53 <74829865+Jayz53@users.noreply.github.com>
2020-12-04 03:39:00 +0500

    Add files via upload

---
+++ play.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, StyleSheet, Text, View  } from 'react-native';
+import { BannerAd, BannerAdSize} from '@react-native-firebase/admob';
+
+const rock = require('../assets/r2.png')
+const paper = require('../assets/p1.png')
+const scissor = require('../assets/s1.png')
+
+const randomRPS = () => {
+  const signs = [rock, scissor, paper];
+  const rand = Math.floor(Math.random()*3);
+  return signs [rand];
+};
+
+
+const bgColors = ['#783937ff', '#28334AFF', '#006747FF', ]
+
+const Play = () => {
+
+  const [counter, setCounter] = useState(3)
+  useEffect(() => {
+    if (counter === 0) return
+    const timer = setTimeout(() => {
+      setCounter(counter - 1)
+    }, 500);
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [counter])
+
+  return (
+    <View style={ StyleSheet.compose(styles.container, {backgroundColor: bgColors[counter - 1]})}>
+      <View style={styles.bannerH}>
+      <BannerAd  size={BannerAdSize.SMART_BANNER} unitId="ca-app-pub-4009417255688208/8059400372" />
+      </View> 
+      {counter > 0?
+      (<Text style={styles.header}>{counter}</Text>) : (
+       <> 
+       <Image style={styles.img} source={randomRPS()} />
+
+       <View style={styles.btn}>
+       <Button  onPress={()=> setCounter(3)} title="Play Again" />
+       </View>
+      </>
+      )}   
+         <View style={styles.bannerF}>
+      <BannerAd size={BannerAdSize.SMART_BANNER} unitId="ca-app-pub-4009417255688208/8059400372" />
+      </View>
+
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+   header: {
+    fontSize: 200,
+    fontWeight: "bold",
+    color: "#fff"
+
+  },
+  img: {
+    width: 350,
+    height: 350,
+    backgroundColor: '#fff',
+    marginBottom: -100,
+    
+  },
+  btn: {
+    position: "absolute",
+    bottom: 130,
+    width: 150,
+  },
+   bannerH: {
+    marginTop: -400,
+    padding: 60
+  },
+   bannerF: {
+    marginBottom: -560,
+    padding: 225
+    }
+});
+
+export default Play;
