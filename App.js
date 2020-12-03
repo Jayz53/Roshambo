27d3b39f120a5d0be7a126d391f39de05fe858bc
Jayz53 <74829865+Jayz53@users.noreply.github.com>
2020-12-04 03:37:24 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Button, Image, StyleSheet, Text, View  } from 'react-native'
 import Play from './screens/play';
-import { BannerAd, BannerAdSize, InterstitialAd, AdEventType} from '@react-native-firebase/admob';
 
 export default function App() {
-    useEffect(() => {
-  const interstitial = InterstitialAd.createForAdRequest("ca-app-pub-4009417255688208/6808369203", {
-  requestNonPersonalizedAdsOnly: true,
-  keywords: ['fashion', 'clothing'],
-});
-let interstitialListener= interstitial.onAdEvent(type => {
-  if(type === AdEventType.LOADED){
-    interstitial.show()
-  }
-});
-interstitial.load();
-
-return()=>{
-interstitialListener=null;
-}
-}, []);
-
+    
 const [play, setPlay] = useState(false)
   return (
     play ? ( <Play />) : (
       
         <View style={styles.container}>
           <View style={styles.banner}>
-        <BannerAd size={BannerAdSize.SMART_BANNER} unitId="ca-app-pub-4009417255688208/8059400372" />
+        
       </View>
       <Image source={require('./assets/main.jpg')} />
       <Text style={styles.header}> Rock      Paper    Scissor</Text>
